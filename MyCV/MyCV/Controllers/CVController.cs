1db3297b4a54f6317df098ab8684cf5b58098c7d
shartazkhan <akashshartazkhan@gmail.com>
2022-06-25 13:53:35 +0600

    initial commit

---
+++ MyCV/MyCV/Controllers/CVController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCV.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Personal()
+        {
+            return View();
+        }
+
+        public ActionResult Academic()
+        {
+            return View();
+        }
+
+    }
+}
